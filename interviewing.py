61dcdf526a8c21ce34b15fac364158d7d97a680f
Alex Fortin <gitbranchlist@gmail.com>
2017-07-23 22:01:59 -0500

    finished front-end

--- interviewing.py
+++ interviewing.py
@@ -6,7 +6,7 @@ Web-app to record feedback about a candidate met during an interview at AmFam
 """
 import flask, os
 from datetime import date
-from flask import Flask, session, render_template, redirect, url_for, abort
+from flask import Flask, session, render_template, redirect, url_for, abort, request
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or \
@@ -16,7 +16,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or \
 def index():
     #It's the user's first visit to the website
     print 'This is a GET request'
-    session['progress_count']=0
+    print session
+    if 'progress_count' not in session:
+        session['progress_count']=0
     return render_template( 'interviewing.html',
                             Progress=session['progress_count'],
                             DisplayName='Interviewing')
@@ -24,9 +26,22 @@ def index():
 @app.route('/interviewing', methods=['POST'])
 def interviewing():
     print 'This is a POST request. Count:{}.'.format(session['progress_count'])
-    return render_template( 'interviewing.html',
-                            Progress=session['progress_count'],
-                            DisplayName='Interviewing')
+    print flask.request.form
+    if 'next_button' in flask.request.form:
+        session['progress_count']+=1
+        return redirect(url_for('index'))
+    elif 'prev_button' in flask.request.form:
+        session['progress_count']= session['progress_count'] - 1
+        return redirect(url_for('index'))
+    elif 'submit_button' in flask.request.form:
+        return redirect(url_for('thankyou'))
+
+@app.route('/interviewing/thankyou', methods=['GET'])
+def thankyou():
+    # delete session
+    # post session to DB
+    return render_template( 'thankyou.html',
+                            IntervieweeName='John Smith')
 
 # SERVING
 if __name__ == '__main__':
