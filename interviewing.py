9df3404ce232c079319416d7ab5fdef69fbf94fc
Alex Fortin <gitbranchlist@gmail.com>
2017-07-23 19:31:02 -0500

    added cool session feature and neat interviewing.hmtl

--- interviewing.py
+++ interviewing.py
@@ -4,16 +4,29 @@ Sunday, July 23rd 2017
 Description
 Web-app to record feedback about a candidate met during an interview at AmFam
 """
-import flask, os.path, imp
+import flask, os
 from datetime import date
-from flask import Flask, render_template, redirect, url_for, abort
+from flask import Flask, session, render_template, redirect, url_for, abort
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-@app.route('/interviewing', methods=['GET','POST'])
-#@auth.login_required
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or \
+    'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
+
+@app.route('/interviewing', methods=['GET'])
+def index():
+    #It's the user's first visit to the website
+    print 'This is a GET request'
+    session['progress_count']=0
+    return render_template( 'interviewing.html',
+                            Progress=session['progress_count'],
+                            DisplayName='Interviewing')
+
+@app.route('/interviewing', methods=['POST'])
 def interviewing():
-    return render_template('interviewing.html', slogan='Yay!', DisplayName='Interviewing')
+    print 'This is a POST request. Count:{}.'.format(session['progress_count'])
+    return render_template( 'interviewing.html',
+                            Progress=session['progress_count'],
+                            DisplayName='Interviewing')
 
 # SERVING
 if __name__ == '__main__':
